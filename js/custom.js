dea86c36b58e6be67a27c4555241075408fa3f07
Cowboy <devinhicks.az@gmail.com>
2024-06-09 14:27:14 -0400

    Redesign site

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,57 @@
+/**********************************************
+ *    MAKE NAVIGATION ACTIVE WHEN SCROLLING
+ * ********************************************/
+
+const sections = document.querySelectorAll("section");
+
+const navLi = document.querySelectorAll(".navbar .container .collapse ul li");
+
+window.onscroll = () => {
+    var current = "";
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (pageYOffset >= sectionTop - 100) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLi.forEach((li) => {
+        li.classList.remove("active");
+        if (li.classList.contains(current)) {
+            li.classList.add("active");
+        }
+    });
+};
+
+/**********************************************
+ *        SMOOTH SCROLLING TO ANCHORS
+ * ********************************************/
 
 
-  /*-------------------------------------------------------------------------------
-    PRE LOADER
-  -------------------------------------------------------------------------------*/
+function smoothScroll() {
+    let x = document.querySelectorAll('li a[href*="#"]');
 
-  $(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
-  });
+    let set = document.getElementsByClassName('navbar-default');
+    let nav = set[0];
 
+    for (let i = 0; i < x.length; i++) {
+        x[i].addEventListener('click', function (e) {
+            e.preventDefault();
+
+            let target = document.querySelector(this.hash);
+
+            let yOffset = -150;
+
+            if (nav.classList.contains("sticky")) {
+                yOffset = -50;
+            }
+
+            const y = target.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+            window.scrollTo({ top: y, behavior: 'smooth' });           
+        });
+    };
+}
+
+smoothScroll();
